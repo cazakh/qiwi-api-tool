6a554969834415f847658a6144d1259742942747
Fedor Sokolov <75084104+cazakh@users.noreply.github.com>
2021-12-30 19:57:16 +0300

    auth

--- GoGo.py
+++ GoGo.py
@@ -1,8 +1,42 @@
 import QiwiMaster
 #API FROM TO
+
+
+from keyauth import api
+
+import os
+import sys
+import os.path
+import platform
+from datetime import datetime
+
+
+keyauthapp = api("QChain", "u6NQ037Tro", "7528c35cc917bd84489bb861bdcdabdc1287128614dc2aa2983f1b5fbe4d6515","1.0")
+
+print("Initializing")
+keyauthapp.init()
+
+
+key = input('Enter your license: ')
+keyauthapp.license(key)
+
+
+print("\nUser data: ") 
+print("Hardware-Id: " + keyauthapp.user_data.hwid)
+print("Created at: " + datetime.utcfromtimestamp(int(keyauthapp.user_data.createdate)).strftime('%Y-%m-%d %H:%M:%S'))
+print("Last login at: " + datetime.utcfromtimestamp(int(keyauthapp.user_data.lastlogin)).strftime('%Y-%m-%d %H:%M:%S'))
+print("Expires at: " + datetime.utcfromtimestamp(int(keyauthapp.user_data.expires)).strftime('%Y-%m-%d %H:%M:%S'))
+
+
+
+
+
+
 f = open("config.txt", 'r')
 n = int(f.readline() )- 1
 otstup = "    "
+
+
 
 getFrom = list(map(str, f.readline().strip().split(' ')))
 for i in range(0, n):
@@ -16,4 +50,4 @@ for i in range(0, n):
     j.write("\n}")
     j.close
     getFrom = putTo
-    QiwiMaster.summa()
+    #QiwiMaster.summa()
